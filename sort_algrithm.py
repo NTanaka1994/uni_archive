c0235b8cf656bbe2a19ff025c84dc43406f31509
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2026-02-03 12:37:29 +0900

    Update sort_algrithm.py

--- sort_algrithm.py
+++ sort_algrithm.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
-
 def babble(a, length):
     for i in range(length-1):
         for j in range(length-i-1):
@@ -19,13 +18,14 @@ def insertion(a, n):
             a[j+1] = a[j]
             j = j - 1
         a[j+1] = temp
-        print(a)
-        
-def quick(a, first, last):
+        print(a)  
+maxdepth = 0
+def quick(a, first, last, depth):
     cnt = 0
+    global maxdepth
+    if maxdepth < depth:
+        maxdepth = depth
     pivot = a[(first + last) // 2]
-    #print(pivot)
-    #print(a[first:last+1])
     i = first
     j = last
     while(True):
@@ -41,14 +41,11 @@ def quick(a, first, last):
         cnt = cnt + 1
         i = i + 1
         j = j - 1
-        #print(a[first:last+1])
-        #print(a)
     if first < (i - 1):
-        cnt = cnt + quick(a, first, i-1)
+        cnt = cnt + quick(a, first, i-1, depth+1)[0]
     if (j + 1) < last:
-        cnt = cnt + quick(a, j+1, last)
-    #print(a)
-    return cnt
+        cnt = cnt + quick(a, j+1, last, depth+1)[0]
+    return cnt, maxdepth
 
 def rand_array(n):
     rng = np.random.default_rng()
@@ -80,9 +77,13 @@ def saw_array(n):
         a.append(i)
         a.append(n-(i-1))
     return a
+
 cnt_v = []
 cnt_v2 = []
 cnt_s = []
+dep_v = []
+dep_v2 = []
+dep_s = []
 volly = []
 volly2 = []
 saw = []
@@ -91,19 +92,28 @@ for i in range(10, 10000, 100):
     val.append(i)
     a = voly_array(i)
     past = time()
-    cnt_v.append(quick(a, 0, len(a)-1))
+    cnt_v.append(quick(a, 0, len(a)-1, 0)[0])
     now = time()
     volly.append(now-past)
+    maxdepth = 0
+    a = voly_array(i)
+    dep_v.append(quick(a, 0, len(a)-1, 0)[1])
     a = voly2_array(i)
     past = time()
-    cnt_v2.append(quick(a, 0, len(a)-1))
+    cnt_v2.append(quick(a, 0, len(a)-1, 0)[0])
     now = time()
     volly2.append(now-past)
+    maxdepth = 0
+    a = voly2_array(i)
+    dep_v2.append(quick(a, 0, len(a)-1, 0)[1])
     a = saw_array(i)
     past = time()
-    cnt_s.append(quick(a, 0, len(a)-1))
+    cnt_s.append(quick(a, 0, len(a)-1, 0)[0])
     now = time()
     saw.append(now-past)
+    maxdepth = 0
+    a = saw_array(i)
+    dep_s.append(quick(a, 0, len(a)-1, 0)[1])
     
 plt.scatter(val, volly, label="Volley(V-pattern)")
 plt.scatter(val, volly2, label="Volley(High2Low)")
@@ -121,11 +131,13 @@ plt.legend()
 plt.xlabel("Number of elements")
 plt.ylabel("Number of swap")
 plt.savefig("quicksort_swap.png")
-plt.show() 
+plt.show()
 
-#a = voly2_array(10)
-#a = [10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 5]
-#a = [10, 8, 6, 4 , 2, 0, 1, 3, 5, 7, 9]
-#a = saw_array(10)
-#print(a)
-#quick(a, 0, len(a)-1)
+plt.scatter(val, dep_v, label="Volley(V-pattern)")
+plt.scatter(val, dep_v2, label="Volley(High2Low)")
+plt.scatter(val, dep_s, label="saw")
+plt.legend()
+plt.xlabel("Number of elements")
+plt.ylabel("Number of depth")
+plt.savefig("quicksort_depth.png")
+plt.show()
