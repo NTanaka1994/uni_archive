8f4f96473389ae0cae98c97f6859f50c310e67be
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2026-03-17 12:52:04 +0900

    Add files via upload

--- 実験5/実験5-2/app2.py
+++ 実験5/実験5-2/app2.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import json
 import requests
+import html
 
 app = Flask(__name__)
 
@@ -14,7 +15,7 @@ def result():
     req = requests.get("https://ja.dbpedia.org/data/"+keyword+".json")
     jsn = json.loads(req.text)
     abstract = jsn["http://ja.dbpedia.org/resource/"+keyword]["http://www.w3.org/2000/01/rdf-schema#comment"][0]["value"]
-    return "<h1>検索結果</h1>\n" + abstract
+    return "<h1>検索結果</h1>\n" + html.escape(abstract)
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
