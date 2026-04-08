e6666ec4cf18934f361c0e749640b1ac86179630
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2026-04-08 12:40:10 +0900

    Update dump.c

--- dump.c
+++ dump.c
@@ -2,17 +2,17 @@
 #include <stdio.h>
 
 void show_line() {
-    printf("ADDRESS  00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15  0123456789ABCDEF\n");
-    printf("======== == == == == == == == == == == == == == == == ==  ================\n");
+    printf(" ADDRESS  00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15  0123456789ABCDEF\n");
+    printf("========= == == == == == == == == == == == == == == == ==  ================\n");
 }
 int main() {
 	FILE *fp = fopen("tasks.json", "rb");
 	unsigned char buf[16];
     size_t n;
     show_line();
-    int j = 0;
+    size_t j = 0;
     while ((n = fread(buf, 1, sizeof(buf), fp)) > 0) {
-        printf("%08X ", j);
+        printf("%09X ", j);
         size_t i;
         for (i = 0; i < n; i++) {
             printf("%02X ", buf[i]);
@@ -24,13 +24,16 @@ int main() {
         printf(" ");
         for (size_t i = 0; i < n; i++) {
             if (buf[i] == '\r') {
-                printf("\\r");
+                //printf("\\r");
+                printf(".");
             }
             else if (buf[i] == '\n') {
-                printf("\\n");
+                //printf("\\n");
+                printf(".");
             }
             else if (buf[i] == '\t') {
-                printf("\\t");
+                //printf("\\t");
+                printf(".");
             }
             else {
                 printf("%c", buf[i]);
